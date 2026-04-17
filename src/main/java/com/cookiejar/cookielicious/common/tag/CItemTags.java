52ee0e004d21dfe71ee0b3b186ef0ff84fef05ea
Sarinsa <mikkemusbruker@gmail.com>
2026-04-17 12:11:21 +0200

    Refactored tag "forge:cookies" into "neoforge:cookies"

--- src/main/java/com/cookiejar/cookielicious/common/tag/CItemTags.java
+++ src/main/java/com/cookiejar/cookielicious/common/tag/CItemTags.java
@@ -5,18 +5,19 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
 
 public class CItemTags {
 
-    public static final TagKey<Item> COOKIES = forgeTag("cookies");
-    public static final TagKey<Item> CHOCOLATE_COOKIES = forgeTag("cookies/chocolate");
+    public static final TagKey<Item> COOKIES = neoforgeTag("cookies");
+    public static final TagKey<Item> CHOCOLATE_COOKIES = neoforgeTag("cookies/chocolate");
 
 
-    private static TagKey<Item> forgeTag(String path) {
-        return ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", path));
+    private static TagKey<Item> neoforgeTag(String path) {
+        return ItemTags.create(ResourceLocation.fromNamespaceAndPath(NeoForgeVersion.MOD_ID, path));
     }
 
     private static TagKey<Item> modTag(String path) {
-        return ItemTags.create(Cookielicious.modPrefix(path));
+        return ItemTags.create(Cookielicious.rl(path));
     }
 }
